113e7731b77d9d64e23439023975c29b4cd3f9d1
Jonayet-06 <jonayethossain3484@gmail.com>
2025-11-18 12:31:28 +0600

    added environment file and index.js file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// console.log(process.env);
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u65jfbo.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
